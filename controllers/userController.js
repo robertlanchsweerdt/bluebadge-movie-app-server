84ce21b2ed191cfdf5c56e530b9534c654af2edc
amber renfro <amber.renfro2021@gmail.com>
2021-09-16 16:46:25 -0500

    created login endpoint in userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -34,7 +34,44 @@ try {
 
 // LOGIN //
 router.post("/login", async (req, res) => {
+    const { username, password } = req.body;
 
+    try {
+        let loginUser = await UserModel.findOne({
+            where: {
+                username: username,
+            },
+        });
+
+        if (loginUser) {
+            let passwordComparison = await bcrypt.compare(password, loginUser.password);
+
+            if (passwordComparison) {
+                let token = jwt.sign({ id: loginUser.id }, process.env.JWT_SECRET, { expiresIn: 60 * 60 * 24 });
+
+                res.status(200).json({
+                    user: loginUser,
+                    message: "Succesfully logged in!",
+                    token: token
+                });
+            } else {
+                res.status(400).json({
+                    message: "Incorrect username or password"
+                });
+            }
+        } else {
+            res.status(400).json({
+                message: "Incorrect username or password"
+            });
+        }
+    
+    } catch (e) {
+        res.status(500).json({
+            message: "oops looks like something went wrong",
+            error
+        })
+    }
 });
+
 
 module.exports = router;
